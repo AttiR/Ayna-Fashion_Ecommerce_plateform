2d837727420d8f908db6ffefa70a7f1994b4a578
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-23 14:18:27 +0200

    Redux setup initial commit

---
+++ frontend/src/store.js
@@ -0,0 +1,6 @@
+// redux store wehere we connect reducers, middleware etc
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
