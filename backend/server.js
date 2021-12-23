ad4285ada7efa84e1806a5a08b629027d93e5bc6
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-23 13:22:30 +0200

    Custom error handling and postman routes testing

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,35 @@
 import express from 'express';
-import dotenv from 'dotenv'
-import products from './data/products.js';
+import dotenv from 'dotenv';
+//import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
-connectDB()
-
-
-
-
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running.....');
 });
-
-app.get('/api/products', (req, res) => {
+// We are moving routes to routes folder we will ftech products routes from productRoutes
+/*app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
 app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
-});
+});*/
+
+// mount the prodcutRoutes
+app.use('/api/products', productRoutes);
+
+// Custom Error Handling
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
