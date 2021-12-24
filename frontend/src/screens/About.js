6a4802a0af13a2f10ca8bb0fb26d12b6e644cc72
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-24 13:24:25 +0200

    About us component style

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="about-section mt-5" id="about">
+        <div className="container">
+          <div className="about">
+            <div className="about-text">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Similique perferendis doloremque officia possimus magnam quo
+                facilis aliquid recusandae eveniet repudiandae corrupti
+                architecto itaque mollitia, beatae necessitatibus vitae cum
+                laboriosam tempora. Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Similique perferendis doloremque officia
+                possimus magnam quo facilis aliquid recusandae eveniet
+                repudiandae corrupti architecto itaque mollitia, beatae
+                necessitatibus vitae cum laboriosam tempora!
+              </p>
+              <Link to="/contact" className="btn btn-info">
+                Get a quote
+              </Link>
+            </div>
+            <div class="about-image">
+              <img src="/images1/image11.jpg" alt="img" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
