33674f7ae63a2d26812f69b3ec39b35edfd4f0ab
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-25 12:19:07 +0200

    Controllers for routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+// import Product Model
+import Product from '../models/productModel.js';
+import asyncHandler from 'express-async-handler';
+
+// in order to handle exceptions insted of try catch, using expressasynchandler
+
+//@desc     Fetch all Products
+//@route  Get /api/products
+//@access public
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}); // {} object will ftech all data
+  res.json(products);
+});
+
+//@desc     Fetch sinle Product
+//@route  Get /api/products/:id
+//@access public
+
+const getProductByID = asyncHandler(async (req, res) => {
+  //const product = products.find((p) => p._id === req.params.id);
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    //res.status(404).json({ message: 'Product not found' });
+    res.status(404);
+    throw new Error('Product Not found');
+  }
+});
+
+export { getProducts, getProductByID };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,42 +1,12 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
+// import from controllers
+import { getProducts, getProductByID } from '../controllers/productController.js';
 
-// import Product Model
-import Product from '../models/productModel.js';
-// in order to handle exceptions insted of try catch, using expressasynchandler
+// Products routes
 
-//@desc     Fetch all Products
-//@route  Get /api/products
-//@access public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); // {} object will ftech all data
-    res.json(products);
-  })
-);
-
-//@desc     Fetch sinle Product
-//@route  Get /api/products/:id
-//@access public
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    //const product = products.find((p) => p._id === req.params.id);
-
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      //res.status(404).json({ message: 'Product not found' });
-      res.status(404)
-      throw new Error('Product Not found')
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductByID);
 
 export default router;
 
