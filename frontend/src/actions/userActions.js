30b9b1d0e584b81b498f2100d3afcad473ac17d3
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-26 17:32:00 +0200

    Show User in Nvabar and Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,42 +1,54 @@
-import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL} from '../constants/userConstants'
-import axios from 'axios'
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from '../constants/userConstants';
+import axios from 'axios';
 
-export const login = (email,password) => async(dispatch) =>{ // redux-thunk
+export const login = (email, password) => async (dispatch) => {
+  // redux-thunk
 
-    try {
-        dispatch({
-            type:USER_LOGIN_REQUEST
-        })
-        // we are sending data into headers 
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+    // we are sending data into headers
 
-        const config = {
-            Headers:{
-                'content-Type': 'Application/json'
+    const config = {
+      Headers: {
+        'content-Type': 'Application/json',
+      },
+    };
 
-            }
-        }
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      { config }
+    );
 
-        const { data } = await axios.post('/api/users/login', {email, password}, {config})
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-        })
+    // storage data in local storage
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message, // ternary operator if else
+    });
+  }
+};
 
-        // storage data in local storage
-        localStorage.setItem('userInfo', JSON.stringify(data))
-    } catch (error) {
-
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-              error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message, // ternary operator if else
-          });
-        
-    }
-
-
-
-}
+// Logout
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({
+    type: USER_LOGOUT,
+  });
+};
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {logout} from '../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar className="navbar" expand="lg" fixed="top">
@@ -28,10 +38,22 @@ const Header = () => {
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Nav.Link as={Link} to="/login">
-                {' '}
-                <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <Nav.Link as={Link} to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </Nav.Link>
+
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              )}
+
               <Nav.Link as={Link} to="/cart">
                 {' '}
                 <i className="fas fa-shopping-cart"></i> Cart
