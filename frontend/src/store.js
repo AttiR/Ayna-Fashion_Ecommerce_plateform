41984a3ab17cdc492b28ca6fc9630e8ccbcd78b0
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-30 14:21:58 +0200

    Orders Details Reducers & actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,7 +15,10 @@ import {
   userUpdateProfileReducer,
 } from './reducers/usersReducers';
 
-import {orderCreateReducer} from './reducers/orderReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer, // productList shows the state
@@ -25,7 +28,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-  orderCreate:orderCreateReducer
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
