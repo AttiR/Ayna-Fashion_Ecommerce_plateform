b411f9300fc6a1125996db26c817205a490b3045
AttiR <77799634+AttiR@users.noreply.github.com>
2022-01-04 22:04:39 +0200

    Create & Update Product Endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,23 @@
 import express from 'express';
 const router = express.Router();
 // import from controllers
-import { getProducts, getProductByID , deleteProduct} from '../controllers/productController.js';
-import { protect, admin } from '../middleware/authMiddleware.js'
+import {
+  getProducts,
+  getProductByID,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // Products routes
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductByID).delete(protect, admin, deleteProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductByID)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
 
